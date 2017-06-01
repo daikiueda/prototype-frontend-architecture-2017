38703dd08d6a05d3140b4c3c265eaa03b771c1da
daikiueda, @ue_di <daikiueda@users.noreply.github.com>
2017-06-02 01:47:34 +0900

    [WIP] entityごとの集約はMap（ImmutableJS）で実装
    * テスト追加
    * 仮置きしたreducer(module)を削除

--- src/TooMuchTodo/store/modules/entities/todoLists.js
+++ src/TooMuchTodo/store/modules/entities/todoLists.js
@@ -1,12 +1,12 @@
-import { List } from 'immutable';
+import { Map } from 'immutable';
 import { createAction } from 'redux-actions';
 
 import TodoList from '../../../domain/models/TodoList';
 
-const INITIAL_STATE = List();
+const INITIAL_STATE = Map();
 
-const REDUCER__CREATE_TODO_LIST = 'TooMuchTodo/toTOGroups/REDUCER__CREATE_TODO_LIST';
-const REDUCER__ADD_TODO_LIST = 'TooMuchTodo/toTOGroups/REDUCER__ADD_TODO_LIST';
+const REDUCER__CREATE_TODO_LIST = 'TooMuchTodo/todoLists/REDUCER__CREATE_TODO_LIST';
+const REDUCER__ADD_TODO_LIST = 'TooMuchTodo/todoLists/REDUCER__ADD_TODO_LIST';
 
 export const actions = {
   createTodoList: createAction(REDUCER__CREATE_TODO_LIST),
@@ -15,11 +15,13 @@ export const actions = {
 
 const processes = {
   [REDUCER__CREATE_TODO_LIST]: (state) => {
-    // Todo
-    return state.push(new TodoList({ id: Date.now() }));
+    // Todo 要否検討：生成してストアに投入する処理だと、使い所がないかも…
+    const todoList = new TodoList({ id: Date.now() });
+    return state.set(todoList.id, todoList);
   },
+
   [REDUCER__ADD_TODO_LIST]: (state, payload) => {
-    return state.push(payload);
+    return state.set(payload.id, payload);
   },
 };
 
---
+++ test/ava/TooMuchTodo/store/modules/entities/todoList.test.js
@@ -0,0 +1,33 @@
+import test from 'ava';
+import { Map } from 'immutable';
+
+import reducer, { actions } from 'TooMuchTodo/store/modules/entities/todoLists';
+
+import TodoList from 'TooMuchTodo/domain/models/TodoList';
+
+test('actions/addTodoList', (t) => {
+  t.deepEqual(
+    actions.addTodoList(),
+    { type: 'TooMuchTodo/todoLists/REDUCER__ADD_TODO_LIST' },
+  );
+});
+
+test('modules/REDUCER__ADD_REPORT', (t) => {
+  const action = {
+    type: 'TooMuchTodo/todoLists/REDUCER__ADD_TODO_LIST',
+    payload: new TodoList({ id: 1111 }),
+  };
+
+  t.deepEqual(
+    reducer(Map(), action).toJSON(),
+    {
+      1111: {
+        description: '',
+        id: 1111,
+        isSaved: false,
+        title: '',
+        todos: [],
+      },
+    },
+  );
+});
