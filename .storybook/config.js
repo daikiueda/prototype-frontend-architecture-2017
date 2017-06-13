30173f1ce1f19de895c8d1e151b01dd2f92038f3
daikiueda, @ue_di <daikiueda@users.noreply.github.com>
2017-06-13 23:23:30 +0900

    [WIP] Storybookでもhtml, body等のスタイルを適用する

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,6 +3,8 @@ import infoAddon from '@storybook/addon-info';
 
 setAddon(infoAddon);
 
+import '../src/commons/components/foundational-styles.scss';
+
 function loadStories() {
   require('./.story-files');
 }
