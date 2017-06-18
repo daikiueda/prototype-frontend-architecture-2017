817c6682637d4270210eebe95eaba7475f69f46d
daikiueda, @ue_di <daikiueda@users.noreply.github.com>
2017-06-18 15:21:48 +0900

    [WIP] Table要素のスタイルにバリエーションを持たせる方針

--- src/commons/components/elements/Table.js
+++ src/commons/components/elements/Table.js
@@ -2,12 +2,22 @@ import BaseElement from './BaseElement';
 
 import './Table.scss';
 
+const CSS_ROOT = 'commons--elements--table';
+
 export default class Table extends BaseElement {
   static get elementType() {
     return 'table';
   }
 
   static get CSS_ROOT() {
-    return 'commons--elements__table';
+    return CSS_ROOT;
+  }
+
+  static get CLASS_NAME() {
+    return {
+      TR: {
+        CHECKED: `${CSS_ROOT}__tr--checked`,
+      },
+    };
   }
 }
