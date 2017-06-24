444c71045893f29b98192d45ae4dbe6c85c8116d
daikiueda, @ue_di <daikiueda@users.noreply.github.com>
2017-06-25 02:41:49 +0900

    [WIP] <Dialog>作り込み。装飾は適当

--- src/commons/components/frames/.stories/Dialog.story.js
+++ src/commons/components/frames/.stories/Dialog.story.js
@@ -1,25 +1,99 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { LIPSUM } from '../../../../../.storybook/utils';
+import { LIPSUM, LIPSUM_JA } from '../../../../../.storybook/utils';
 
+import Button from '../../elements/Button';
 import Global from '../Global';
 import Dialog from '../Dialog';
+
+const dummyBody = [
+  <p><strong>Dialog</strong></p>,
+  <p>{LIPSUM_JA}</p>,
+];
 
 storiesOf('commons/frames', module)
   .addWithInfo(
     'Dialog',
+    () => (
+      <div className="stage">
+        <Dialog>{dummyBody}</Dialog>
+      </div>
+    ),
+    { inline: true, source: true, propTables: [Dialog] },
+  )
+  .addWithInfo(
+    'Dialog with title and headerAside',
+    () => (
+      <div className="stage">
+        <Dialog
+          title="Title Text"
+          headerAside={
+            <div><Button>ダミーボタン</Button></div>
+          }
+        >{dummyBody}</Dialog>
+      </div>
+    ),
+    { inline: true, source: true, propTables: [Dialog] },
+  )
+  .addWithInfo(
+    'Dialog with headerAside Only',
+    () => (
+      <div className="stage">
+        <Dialog
+          headerAside={
+            <div><Button>ダミーボタン</Button></div>
+          }
+        >{dummyBody}</Dialog>
+      </div>
+    ),
+    { inline: true, source: true, propTables: [Dialog] },
+  )
+  .addWithInfo(
+    'Dialog with footer',
+    () => (
+      <div className="stage">
+        <Dialog
+          footer={
+            <div><Button>ダミーボタンA</Button></div>
+          }
+        >{dummyBody}</Dialog>
+      </div>
+    ),
+  )
+  .addWithInfo(
+    'Dialog with footer and footerAside',
+    () => (
+      <div className="stage">
+        <Dialog
+          footer={
+            <div><Button>ダミーボタンA</Button></div>
+          }
+          footerAside={
+            <div><Button>ダミーボタンB</Button></div>
+          }
+        >{dummyBody}</Dialog>
+      </div>
+    ),
+    { inline: true, source: true, propTables: [Dialog] },
+  )
+  .addWithInfo(
+    'Dialog with footerAside only',
+    () => (
+      <div className="stage">
+        <Dialog
+          footerAside={
+            <div><Button>ダミーボタンB</Button></div>
+          }
+        >{dummyBody}</Dialog>
+      </div>
+    ),
+    { inline: true, source: true, propTables: [Dialog] },
+  )
+  .addWithInfo(
+    'Dialog (as modal)',
     () => {
       const renderModal = () => (
-        <Dialog>
-          <div>
-            <p><strong>Modal Dialog</strong></p>
-            <p>{LIPSUM}</p>
-            <p>{LIPSUM}</p>
-            <p>{LIPSUM}</p>
-            <p>{LIPSUM}</p>
-            <p>{LIPSUM}</p>
-          </div>
-        </Dialog>
+        <Dialog>{dummyBody}</Dialog>
       );
 
       return (
@@ -34,5 +108,5 @@ storiesOf('commons/frames', module)
         </div>
       );
     },
-    { inline: true, source: true, propTables: [Global] },
+    { inline: true, source: true, propTables: [Dialog] },
   );
--- src/commons/components/frames/Dialog.js
+++ src/commons/components/frames/Dialog.js
@@ -9,6 +9,13 @@ const CSS_ROOT = 'commons--frame--dialog';
 export default class Dialog extends React.Component {
   static get propTypes() {
     return {
+      title: PropTypes.oneOfType([
+        PropTypes.element,
+        PropTypes.string,
+      ]),
+      headerAside: PropTypes.element,
+      footer: PropTypes.element,
+      footerAside: PropTypes.element,
       children: PropTypes.oneOfType([
         PropTypes.element,
         PropTypes.arrayOf(PropTypes.element),
@@ -19,22 +26,47 @@ export default class Dialog extends React.Component {
 
   static get defaultProps() {
     return {
+      title: null,
+      headerAside: null,
+      footer: null,
+      footerAside: null,
       className: '',
     };
   }
 
   renderHeader() {
+    const titleElm = this.props.title && typeof this.props.title === 'string'
+      ? <h1 className={`${CSS_ROOT}__title`}>{this.props.title}</h1>
+      : this.props.title;
+    const asideElm = this.props.headerAside
+      && <div className={`${CSS_ROOT}__header-aside`}>{this.props.headerAside}</div>;
+
+    if (!titleElm && !asideElm) {
+      return null;
+    }
+
     return (
-      <header>
-        <h1>Header Text</h1>
+      <header className={titleElm ? '' : `${CSS_ROOT}__header--aside-only`}>
+        {titleElm}
+        {asideElm}
       </header>
     );
   }
 
   renderFooter() {
+    const mainElm = this.props.footer
+      && <div className={`${CSS_ROOT}__footer-main`}>{this.props.footer}</div>;
+    const asideElm = this.props.footerAside
+      && <div className={`${CSS_ROOT}__footer-aside`}>{this.props.footerAside}</div>;
+
+    if (!mainElm && !asideElm) {
+      return null;
+    }
+
     return (
-      <footer>
-        Footer
+      <footer className={asideElm ? `${CSS_ROOT}__footer--has-aside` : ''}>
+        {asideElm}
+        {mainElm}
       </footer>
     );
   }
@@ -44,7 +76,7 @@ export default class Dialog extends React.Component {
       <div className={classNames(CSS_ROOT, this.props.className)}>
         <section>
           {this.renderHeader()}
-          {this.props.children}
+          <div className={`${CSS_ROOT}__body`}>{this.props.children}</div>
           {this.renderFooter()}
         </section>
       </div>
