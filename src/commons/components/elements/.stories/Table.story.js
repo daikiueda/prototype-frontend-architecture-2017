7c57b6a1e78741d3535c806bc65b118e43f41d58
daikiueda, @ue_di <daikiueda@users.noreply.github.com>
2017-06-25 11:39:57 +0900

    [WIP] <Table>表示調整

--- src/commons/components/elements/.stories/Table.story.js
+++ src/commons/components/elements/.stories/Table.story.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { LIPSUM } from '../../../../../.storybook/utils';
 
 import Table from '../Table';
 
@@ -7,10 +8,20 @@ storiesOf('commons/elements', module)
   .addWithInfo(
     'Table',
     () => (
-      <div className="stage">
+      <div className="stage" style={{ display: 'flex' }}>
         <Table>
-          <tbody><tr><td>aaa</td></tr></tbody>
+          <tbody>
+            <tr><td>AAAA</td></tr>
+            <tr><td>BBBB</td></tr>
+            <tr><td>CCCC</td></tr>
+            <tr><td>DDDD</td></tr>
+            <tr><td>EEEE</td></tr>
+            <tr><td>FFFF</td></tr>
+          </tbody>
         </Table>
+        <div style={{ width: 300, paddingLeft: 14 }}>
+          <p>{LIPSUM}</p>
+        </div>
       </div>
     ),
     { inline: true, source: true, propTables: [Table] },
